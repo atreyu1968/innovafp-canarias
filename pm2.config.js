75b2519a642b2a0447e816e10afb8f82b9f9c1d2
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-10-10 18:35:22 +0100

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,1 +1,14 @@
+{
+  "apps": [
+    {
+      "name": "Innovafp",
+      "script": "index.js",
+      "instances": "max",
+      "exec_mode": "cluster",
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  ]
+}
 
